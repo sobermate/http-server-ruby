edd49115213f3128a194221eda30c00a598caa0c
sobermate <nikhilsc42@gmail.com>
2024-11-25 13:19:33 +0530

    Parse request URL

--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,20 @@
 require "socket"
 
 server = TCPServer.new("localhost", 4221)
-client_socket, client_address = server.accept
+puts "Welcome to Ruby HTTP server"
 
-client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+loop do
+    puts "Taking requests"
+    client = server.accept
+    request = client.gets
+    request_components = request.split(" ")
+    response = ""
+    if request_components[1] != "/"
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    else 
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+    end
+    puts "This is what we are sending #{response}"
+    client.puts response
+    client.close
+end 
